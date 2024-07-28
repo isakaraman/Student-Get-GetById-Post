f4da3bae3754470247805074ac778b78d4969160
İsa Karaman <77787550+isakaraman@users.noreply.github.com>
2024-07-28 14:59:02 +0300

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using BasicWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicWebApi.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class StudentController
+	{
+		static List<Students> students = new List<Students>
+		{
+			new Students{Id=1,Name="Pelorat"},
+			new Students{Id=2,Name="Trevize"},
+			new Students{Id=3,Name="Bliss"}
+
+		};
+
+		[HttpGet]
+		public List<Students> Get()
+		{
+			return students;
+		}
+
+		[HttpGet("{id}")]
+		public Students GetById(int id)
+		{
+			return students.FirstOrDefault(x=>x.Id==id);
+		}
+
+		[HttpPost]
+		public Students Post(Students newStudent)
+		{
+			students.Add(newStudent);
+			return newStudent;
+		}
+		
+	}
+}
---
+++ Models/Students.cs
@@ -0,0 +1,8 @@
+﻿namespace BasicWebApi.Models
+{
+	public class Students
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
